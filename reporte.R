9136b176df03026419148f8d8a1912774e4fe00c
Amado Rosas <amadorosas97@gmail.com>
2024-07-29 14:57:13 -0700

    reescritura de comentarios en español

--- reporte.R
+++ reporte.R
@@ -1,60 +1,61 @@
+# Instalación de paquetes necesarios
 install.packages("openxlsx")
 install.packages("dplyr")
 install.packages("moments")
 install.packages("ggplot2")
 
+# Carga de librerías
 library("openxlsx")
 library("dplyr")
 library("moments")
 library("ggplot2")
 
-# Set the working directory
+# Establecer el directorio de trabajo
 setwd("/Users/imac/Library/CloudStorage/OneDrive-UniversidaddeSonora/University/Trabajo Delfin/Ricardo/Reporte")
 
-# Load the data
+# Cargar los datos
 data <- read.xlsx("tabla datos.xlsx", sheet = 1)
 
-# Change the header names
+# Cambiar los nombres de las columnas
 names(data) <- c("origen", "destino", "dia", "salida", "pronostico", "real")
 
-# Convert dia to Date format
+# Convertir 'dia' a formato Date
 data$dia <- as.Date(data$dia, origin = "1899-12-30")
 
-# Create a data frame with data
+# Crear un data frame con los datos
 data <- data.frame(data)
 View(data)
 
-# Description of the data
+# Descripción resumida de los datos
 summary(data)
 
-# Identify and remove missing values
+# Identificar y eliminar valores faltantes
 data <- na.omit(data)
 
-# Transform categorical variables (origen and destino) to factors
+# Transformar variables categóricas (origen y destino) a factores
 data$origen <- factor(data$origen)
 data$destino <- factor(data$destino)
 
-# Create a new column with the difference between the real and the forecast
+# Crear una nueva columna con la diferencia entre lo real y el pronóstico
 data <- mutate(data, diferencia = real - pronostico)
 
-# Create a new column with the abs value of diferencia
+# Crear una nueva columna con el valor absoluto de la diferencia
 data <- mutate(data, diferencia_abs = abs(diferencia))
 
-## Estatisitcs
-
-# Descriptive statistics (mean, median, mode, variance, standard deviation, skewness, kurtosis)
+## Estadísticas descriptivas
+# Función para calcular estadísticas descriptivas
 descriptive_statistics <- function(dataframe, columna) {
   if(!is.data.frame(dataframe)) {
     stop("El primer argumento debe ser un dataframe.")
   }
 
-  if(!columna %in% colnames(dataframe)) { # nolint: spaces_left_parentheses_linter, line_length_linter.
+  if(!columna %in% colnames(dataframe)) {
     stop("La columna especificada no existe en el dataframe.")
   }
 
   data <- dataframe[[columna]]
 
-  if(!is.numeric(data)) { # nolint
+  if(!is.numeric(data)) {
     stop("La columna debe ser numérica.")
   }
 
@@ -75,6 +76,7 @@ descriptive_statistics <- function(dataframe, columna) {
   return(statistics)
 }
 
+# Estadísticas descriptivas para las variables de interés
 statistics_salida <- descriptive_statistics(data, "salida")
 print(statistics_salida)
 
@@ -88,7 +90,9 @@ statistics_diferencia <- descriptive_statistics(data, "diferencia")
 print(statistics_diferencia)
 
 
-# Boxplot of the distribution of the differences by origen
+## Gráficos
+# Boxplot de la distribución de las diferencias por origen
+# Ayuda a visualizar la variabilidad de las diferencias entre el pronóstico y lo real para cada origen
 ggplot(data, aes(x = origen, y = diferencia)) +
   geom_boxplot(notch = TRUE) +
   ggtitle("Distribución de las diferencias por origen") +
@@ -98,7 +102,8 @@ ggplot(data, aes(x = origen, y = diferencia)) +
 
 ggsave("boxplot_origen.png", plot = last_plot())
 
-# Boxplot of the distribution of the differences by origen
+# Boxplot de la distribución de las diferencias por destino
+# Similar al anterior, pero enfocado en los destinos para identificar si hay diferencias en función del destino
 ggplot(data, aes(x = destino, y = diferencia)) +
   geom_boxplot(notch = TRUE) +
   ggtitle("Distribución de las diferencias por destino") +
@@ -108,30 +113,32 @@ ggplot(data, aes(x = destino, y = diferencia)) +
 
 ggsave("boxplot_destino.png", plot = last_plot())
 
-# Double histogram of the forecast and the real
+# Histograma doble del pronóstico y lo real
+# Permite comparar las distribuciones de los valores pronosticados y los valores reales
 ggplot(data = data, aes(x = pronostico, fill = "Pronóstico")) +
   geom_histogram(binwidth = 1, alpha = 0.5) +
   geom_histogram(aes(x = real, fill = "Real"), binwidth = 1, alpha = 0.5) +
-  labs(title = "Histograma de Pronóstico y Real",
+  labs(title = "Histograma de pronóstico y real",
        x = "Valor",
        y = "Frecuencia") +
   scale_fill_manual(values = c("Pronóstico" = "blue", "Real" = "red"))
 
 ggsave("histograma.png", plot = last_plot())
 
-# Scatter plot of the forecast and the real
+# Gráfico de dispersión del pronóstico y lo real
+# Visualiza la relación entre los valores pronosticados y los valores reales, útil para detectar patrones o discrepancias
 ggplot(data = data, aes(x = pronostico, y = real)) +
   geom_point() +
-  labs(title = "Gráfico de Dispersión de Pronóstico y Real",
+  labs(title = "Gráfico de Dispersión de pronóstico y real",
        x = "Pronóstico",
        y = "Real")
 
 ggsave("scatterplot.png", plot = last_plot())
 
-# Heatmap difference by origen and destino
+# Mapa de calor de la diferencia por origen y destino
+# Visualiza la magnitud de las diferencias absolutas entre pronóstico y real para cada combinación de origen y destino
 ggplot(data = data, aes(x = origen, y = destino, fill = diferencia_abs)) +
     geom_tile(color = "white", linewidth = 0.25) +
-    #geom_text(aes(label = diferencia), color = "white", size = 4) +
     coord_fixed() +
     scale_fill_gradient(low = "grey", high = "black") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10)) +
@@ -142,21 +149,23 @@ ggplot(data = data, aes(x = origen, y = destino, fill = diferencia_abs)) +
 
 ggsave("heatmap.png", plot = last_plot())
 
-# Create a data frame with the data by origen and the sum of the differences
+# Gráfico de barras de la diferencia total por origen
+# Muestra la suma de las diferencias absolutas por cada origen para identificar cuál contribuye más a la discrepancia
 data_origen <- data %>%
   group_by(origen) %>%
   summarise(total_diferencia = sum(diferencia_abs))
 
-# Bar plot of the total difference by origen
 ggplot(data = data_origen, aes(x = origen, y = total_diferencia)) +
   geom_bar(stat = "identity", fill = "#373737") +
-  labs(title = "Diferencia Total por Origen",
+  labs(title = "Diferencia Total por origen",
        x = "Origen",
-       y = "Diferencia Total") +
+       y = "Diferencia total") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
 
 ggsave("barplot_origen.png", plot = last_plot())
 
+# Serie de tiempo de la diferencia total por día
+# Visualiza cómo las diferencias totales varían a lo largo del tiempo
 data_time <- data %>%
   group_by(dia) %>%
   summarise(total_diferencia = sum(diferencia_abs))
@@ -170,7 +179,8 @@ ggplot(data = data_time, aes(x = dia, y = total_diferencia)) +
 
 ggsave("timeseries.png", plot = last_plot())
 
-# Remove outliers (IQR)
+## Análisis de datos
+# Función para eliminar outliers usando el método del IQR
 remove_outliers <- function(dataframe, columna) {
   if(!is.data.frame(dataframe)) {
     stop("El primer argumento debe ser un dataframe.")
@@ -200,3 +210,18 @@ remove_outliers <- function(dataframe, columna) {
   return(dataframe)
 }
 
+## Pruebas estadísticas
+# Prueba t para la diferencia entre el pronóstico y lo real
+# Esta prueba evalúa si hay una diferencia significativa entre los pronósticos y los valores reales
+t_test <- t.test(data$pronostico, data$real, paired = TRUE)
+print(t_test)
+
+# Prueba ANOVA para la diferencia entre el pronóstico y lo real por origen
+# Evalúa si hay diferencias significativas en los horarios de salida entre diferentes orígenes
+anova_test <- aov(salida ~ origen, data = data)
+summary(anova_test)
+
+# Modelo de regresión lineal para evaluar la relación entre la hora de salida y el destino
+# Ayuda a entender si los destinos influyen significativamente en los horarios de salida
+modelo <- lm(salida ~ destino, data = data)
+summary(modelo)
